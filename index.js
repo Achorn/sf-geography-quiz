27e9e2ab15ce3734a320c0425bffe9bd035158ff
achorn <joshuaachorn@gmail.com>
2023-07-27 20:17:37 -0700

    added street toggle

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 
 // console.log(paths);
 //
+
+function handleChange(e, id) {
+  console.log(e);
+}
+
+const streetToggle = document.getElementById("smallStreetSelector");
+const result = document.querySelector(".result");
+
+streetToggle.addEventListener("change", (event) => {
+  let display = document.getElementById("layer2");
+  display.style.display = display.style.display == "inline" ? "none" : "inline";
+});
